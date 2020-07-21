3112ba598b6c6c7bd9b1b8f1496801ec46128114
Oliver Saternus <mail@oliversaternus.de>
2020-07-21 15:57:47 +0200

    fix tests

--- src/posts/test/posts.controller.spec.ts
+++ src/posts/test/posts.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from '../posts.controller';
+import { PostsService } from '../posts.service';
+import { Repository } from 'typeorm';
+import { createMock } from '@golevelup/nestjs-testing';
+import { Post } from '../post.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('Posts Controller', () => {
   let controller: PostsController;
@@ -7,6 +12,13 @@ describe('Posts Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
+      providers: [
+        PostsService,
+        {
+          provide: getRepositoryToken(Post),
+          useValue: createMock<Repository<Post>>(),
+        }
+      ]
     }).compile();
 
     controller = module.get<PostsController>(PostsController);
--- src/users/test/users.controller.spec.ts
+++ src/users/test/users.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from '../users.controller';
+import { UsersService } from '../users.service';
+import { createMock } from '@golevelup/nestjs-testing';
+import { Repository } from 'typeorm';
+import { User } from '../user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('Users Controller', () => {
   let controller: UsersController;
@@ -7,6 +12,12 @@ describe('Users Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: createMock<Repository<User>>(),
+        }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
